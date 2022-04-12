fabba11838ffa02af31726ebc8ab2728ea6e7c41
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-13 00:02:51 +0600

    Add files via upload

---
+++ Print integer number.c
@@ -0,0 +1,11 @@
+/*Write a program that takes an integer number
+and print that number
+*/
+#include<stdio.h>
+int main ()
+{
+    int num;
+    printf("Please enter an integer =");
+    scanf("%d" ,&num);
+    printf("You have pressed = %d\n",num);
+}
