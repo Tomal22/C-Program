044c44fbf73ee682b437ebf1fba8fa17a60d64c3
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-13 00:03:37 +0600

    Add files via upload

---
+++ Print two integer number.c
@@ -0,0 +1,11 @@
+//scanf: two integer number
+#include<stdio.h>
+int main ()
+{
+    int num1,num2;
+    printf("Enter two integers=");
+    scanf("%d %d",&num1,&num2);
+
+    printf("Numbers are=%d %d",num1,num2);
+
+}
