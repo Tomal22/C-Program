88bffaa88eb276a284476e7e1afba75d8f244511
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-13 00:15:55 +0600

    Add files via upload

---
+++ ASCII Value convert.c
@@ -0,0 +1,11 @@
+//ASCII Value converte
+#include<stdio.h>
+int main ()
+{
+    char lower;
+    printf("Enter any lowercase letter=");
+    scanf("%c",&lower); //b=98
+
+    printf("The uppercase letter =%c", lower-32); //B=66
+
+}
