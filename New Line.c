13c67fba51a2a6827a96f694b619ac420603c3de
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-13 00:13:23 +0600

    Add files via upload

---
+++ New Line.c
@@ -0,0 +1,10 @@
+//NEW LINE
+#include<stdio.h>
+int main ()
+{
+    printf("Give me some sunshine \n");
+    printf("Give me some rain\n");
+    printf("Give me another chance\n");
+    printf("I wanna grow up once again");
+    getch();
+}
