8c703ad363f26f970ed567232aa4cbfe0836ad9c
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-13 00:14:26 +0600

    Add files via upload

---
+++ ASCII Value.c
@@ -0,0 +1,11 @@
+//ASCII Value
+#include<stdio.h>
+int main ()
+{
+    int n;
+    printf("Enter any ASCII value=");
+    scanf("%d",&n);
+
+    printf("The ASCII character is=%c",n);
+    getch ();
+}
