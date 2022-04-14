0df83935630ed96048adc1b68f05c99970cc4238
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-14 16:31:30 +0600

    Add files via upload

---
+++ Multiplication.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,mul;
+    printf("Enter two numbers=");
+    scanf("%d%d",&num1,&num2);
+    mul=num1*num2;
+    printf("The mul is=%d\n",mul);
+    getch();
+}
