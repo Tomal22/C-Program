e8c52824d2cbca271e8bdaf491356736a82163bc
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-14 16:09:24 +0600

    Add files via upload

---
+++ Minus.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sub;
+    printf("Enter two numbers=");
+    scanf("%d%d",&num1,&num2);
+    sub=num1-num2;
+    printf("The substruction is=%d\n",sub);
+    getch();
+}
