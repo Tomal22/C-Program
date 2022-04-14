53c3f5accfccf5bda706d035a8fd124825faff29
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-14 15:32:52 +0600

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,11 @@
+//Write a program that takes two integer and display sum
+#include<stdio.h>
+int main()
+{
+    int num1,num2,sum;
+    printf("Two integer num=");
+    scanf("%d%d",&num1,&num2);
+    sum=num1+num2;
+    printf("the sum is=%d\n",sum);
+    getch();
+}
