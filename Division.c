5cfce0cdf3f573371dcaf450a54c627fff233184
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-14 16:32:03 +0600

    Add files via upload

---
+++ Division.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,Div;
+    printf("Enter two number=");
+    scanf("%d%d",&num1,&num2);
+    Div=num1/num2;
+    printf("The division is=%d\n",Div);
+    getch();
+
+}
