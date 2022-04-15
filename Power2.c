a185395554a6ca1f5183ddaafe7f43ea8c538b23
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-15 16:51:57 +0600

    Add files via upload

---
+++ Power2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    printf("enter x=");
+    scanf("%d",&x);
+
+    printf("enter y=");
+    scanf("%d",&y);
+
+    double result=pow(x,y);
+    printf("%lf",result);
+}
+
