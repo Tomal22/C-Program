5212950a5a593591583682f4bfd82c97677e5828
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-15 12:41:24 +0600

    Add files via upload

---
+++ Swap.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num1=10;
+    int num2=20;
+
+    int temp;
+    temp=num1;
+    num1=num2;
+    num2=temp;
+
+    printf("num1=%d\n",num1);
+    printf("num2=%d\n",num2);
+}
