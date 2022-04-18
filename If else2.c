aaa5fa6f51a1c5f1561cef3f0596eb6f114155f1
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-18 20:58:51 +0600

    Add files via upload

---
+++ If else2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int time=9;
+
+    if (time==10)
+    {
+        printf("What time is it?\n");
+        printf("Its 10'O clock\n");
+
+    }
+    else
+    {
+        printf("What time is it?\n");
+        printf("Its 9'O clock");
+    }
+}
