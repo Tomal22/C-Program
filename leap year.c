43305319d4a35919c12dff27ab8813ae1537d4dd
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-20 02:22:38 +0600

    Add files via upload

---
+++ leap year.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Enter any year=");
+    scanf("%d",&year);
+
+    if(year%400==0)
+        printf("Leap year");
+
+    else if(year%4==0 && year%100!=0)
+        printf("Leap year");
+    else
+        printf("Not leap year");
+        getch();
+}
