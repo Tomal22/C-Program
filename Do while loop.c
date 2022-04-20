fa7ba248a8a7f32504c75774461abb8477bd1821
Tanjim Ahmed Tomal <103332941+Tomal22@users.noreply.github.com>
2022-04-20 22:28:10 +0600

    Add files via upload

---
+++ Do while loop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i=10;
+    do
+        {
+            printf("Valar margulis\n");
+            printf("%d\n",i);
+            i++;
+        }
+    while(i<=10);
+    getch();
+}
